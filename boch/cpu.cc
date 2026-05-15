a2b331ffe30c6e3609e80bd3e6921d25468ad74a
libobokabuto <746641854@qq.com>
2026-05-15 11:31:23 +0800

    跑通3001条

--- boch/cpu.cc
+++ boch/cpu.cc
@@ -33,7 +33,7 @@ void BX_CPU_C::cpu_loop(void)
             BX_CPU_THIS_PTR prev_rip = RIP; // commit new RIP
             BX_INSTR_AFTER_EXECUTION(BX_CPU_ID, i);
             BX_CPU_THIS_PTR icount++;
-            if (BX_CPU_THIS_PTR icount == 2213)
+            if (BX_CPU_THIS_PTR icount == 3100)
             {
                 int qwq = 0;
             }
